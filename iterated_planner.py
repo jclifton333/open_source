2bed0f21dd8aa78ef5d9ead58632ca7dd9c98ec6
Jesse Clifton <jclifton333@gmail.com>
2019-11-07 18:44:08 -0500

    start implementing switching policy

--- iterated_planner.py
+++ iterated_planner.py
@@ -28,14 +28,11 @@ def defection_test(player_1_reward_history_, player_2_reward_history_, player_1_
   return player_1_defecting, player_2_defecting
 
 
-class IteratedGamedLearner(metaclass=ABCMeta):
-  def __init__(self, joint_optimization=True,
-               payoffs1=np.array([[-1., -3.], [0., -2.]]), payoffs2=np.array([[-1., -3.], [0., -2.]])):
+class IteratedGameLearner(metaclass=ABCMeta):
+  def __init__(self, payoffs1=np.array([[-1., -3.], [0., -2.]]), payoffs2=np.array([[-1., -3.], [0., -2.]])):
     """
-    Default payoffs are PD.
-
-    :param payoffs1:
-    :param payoffs2:
+    Generic API from which classes for learning policies with policy gradient as well as those for learning switching
+    policies inherit.
     """
     # overwrite these in child classes; **kwargs can be used here
     self.num_params1 = -1
@@ -56,11 +53,6 @@ class IteratedGamedLearner(metaclass=ABCMeta):
     self.action_history = None
     self.state_history = None
     self.ipw_history = None
-    self.joint_optimization = joint_optimization
-
-  @abstractmethod
-  def payoffs(self, params1, params2, ipw_history, reward_history, action_history, state_history):
-    pass
 
   @staticmethod
   def actions_from_params(params1, params2, s1, s2):
@@ -87,6 +79,65 @@ class IteratedGamedLearner(metaclass=ABCMeta):
     r1 = np.random.normal(mu1, 0.1)
     r2 = np.random.normal(mu2, 0.1)
     return r1, r2
+
+  def learn(self,
+            label=0,
+            lr=0.1,
+            updater1=optim.vanilla_gradient,
+            updater2=optim.vanilla_gradient,
+            std=0.01,
+            n_epochs=2000,
+            n_print_every=None,
+            init_params1=None,
+            init_params2=None,
+            plot_learning=True
+            ):
+    # ToDo: many of these params may not be used
+    # Initialize data for learning
+    self.pr_CC_log = np.empty(n_epochs)
+    self.pr_DD_log = np.empty(n_epochs)
+    self.payoffs1_log = np.empty(n_epochs)
+    self.payoffs2_log = np.empty(n_epochs)
+    self.reward_history = []
+    self.action_history = []
+    self.state_history = []
+    self.ipw_history = []
+    self.opponent_reward_estimates_1 = np.zeros((2, 2))  # Agent 2 reward at (a_2, a_1)
+    self.opponent_reward_estimates_2 = np.zeros((2, 2))  # Agent 1 reward at (a_1, a_2)
+    self.opponent_reward_estimate_counts_1 = np.zeros((2, 2))
+    self.opponent_reward_estimate_counts_2 = np.zeros((2, 2))
+    self.current_state = (0, 0) # Start off both cooperating
+
+  def update_histories(self, r1, r2, a1, a2):
+    # Update histories
+    self.state_history.append(self.current_state)
+    self.reward_history.append((r1, r2))
+    self.action_history.append((a1, a2))
+    self.ipw_history.append(ipw)
+    self.opponent_reward_estimate_counts_1[a2, a1] += 1
+    self.opponent_reward_estimate_counts_2[a1, a2] += 1
+    self.opponent_reward_estimates_1[a2, a1] += (r2 - self.opponent_reward_estimates_1[a2, a1]) / \
+    self.opponent_reward_estimate_counts_1[a2, a1]
+    self.opponent_reward_estimates_2[a1, a2] += (r1 - self.opponent_reward_estimates_2[a1, a2]) / \
+    self.opponent_reward_estimate_counts_2[a1, a2]
+    self.current_state = (a2, a1)  # Agent i's state is agent -i's previous action
+
+
+class IteratedGamePGLearner(IteratedGameLearner):
+  def __init__(self, joint_optimization=True,
+               payoffs1=np.array([[-1., -3.], [0., -2.]]), payoffs2=np.array([[-1., -3.], [0., -2.]])):
+    """
+    Default payoffs are PD.
+
+    :param payoffs1:
+    :param payoffs2:
+    """
+    super().__init__(payoffs1=payoffs1, payoffs2=payoffs2)
+    self.joint_optimization = joint_optimization
+
+  @abstractmethod
+  def payoffs(self, params1, params2, ipw_history, reward_history, action_history, state_history):
+    pass
 
   @staticmethod
   def gradient_ascent(lr,
@@ -117,22 +168,9 @@ class IteratedGamedLearner(metaclass=ABCMeta):
             init_params2=None,
             plot_learning=True
             ):
-
-    # Initialize data for learning
-    self.pr_CC_log = np.empty(n_epochs)
-    self.pr_DD_log = np.empty(n_epochs)
-    self.payoffs1_log = np.empty(n_epochs)
-    self.payoffs2_log = np.empty(n_epochs)
-    self.reward_history = []
-    self.action_history = []
-    self.state_history = []
-    self.ipw_history = []
-    self.opponent_reward_estimates_1 = np.zeros((2, 2))  # Agent 2 reward at (a_2, a_1)
-    self.opponent_reward_estimates_2 = np.zeros((2, 2))  # Agent 1 reward at (a_1, a_2)
-    self.opponent_reward_estimate_counts_1 = np.zeros((2, 2))
-    self.opponent_reward_estimate_counts_2 = np.zeros((2, 2))
-    self.current_state = (0, 0) # Start off both cooperating
-
+    super(IteratedGamePGLearner, self).learn(label=label, lr=lr, updater1=updater1, updater2=updater2, std=std,
+                                             n_epochs=n_epochs, n_print_every=n_print_every, init_params1=init_params1,
+                                             init_params2=init_params2, plot_learning=plot_learning)
     params1 = std * T.randn(self.num_params1)
     if init_params1:
       assert len(init_params1) == self.num_params1, \
@@ -155,19 +193,7 @@ class IteratedGamedLearner(metaclass=ABCMeta):
       a1, a2, ipw = self.actions_from_params(params1, params2, self.current_state[0], self.current_state[1])
       r1, r2 = self.outcomes(a1, a2)
 
-      # Update histories
-      self.state_history.append(self.current_state)
-      self.reward_history.append((r1, r2))
-      self.action_history.append((a1, a2))
-      self.ipw_history.append(ipw)
-      self.opponent_reward_estimate_counts_1[a2, a1] += 1
-      self.opponent_reward_estimate_counts_2[a1, a2] += 1
-      self.opponent_reward_estimates_1[a2, a1] += (r2 - self.opponent_reward_estimates_1[a2, a1]) / \
-        self.opponent_reward_estimate_counts_1[a2, a1]
-      self.opponent_reward_estimates_2[a1, a2] += (r1 - self.opponent_reward_estimates_2[a1, a2]) / \
-        self.opponent_reward_estimate_counts_2[a1, a2]
-      self.current_state = (a2, a1) # Agent i's state is agent -i's previous action
-
+      self.update_histories(r1, r2, a1, a2)
       probs1 = T.sigmoid(params1[(2*a2):(2*a2 + 2)]).detach().numpy()
       probs2 = T.sigmoid(params2[(2*a1):(2*a1 + 2)]).detach().numpy()
       probs1 /= np.sum(probs1)
@@ -330,7 +356,7 @@ class IteratedGamedLearner(metaclass=ABCMeta):
     return value_estimate_1 / is_normalizer, value_estimate_2 / is_normalizer
 
 
-class MaxMinSolver(IteratedGamedLearner):
+class MaxMinSolver(IteratedGamePGLearner):
   def __init__(self, payoffs1=np.array([[-1., -3.], [0., -2.]])):
     super().__init__(joint_optimization=False, payoffs1=payoffs1)
     self.num_params1 = 4
@@ -340,7 +366,7 @@ class MaxMinSolver(IteratedGamedLearner):
     return self.value_estimates(params1, params2, ipw_history, reward_history, action_history, state_history)
 
 
-class NashBargainingSolver(IteratedGamedLearner):
+class NashBargainingSolver(IteratedGamePGLearner):
   def __init__(self,
                disagreement_value_1,
                disagreement_value_2,
@@ -357,6 +383,110 @@ class NashBargainingSolver(IteratedGamedLearner):
       self.value_estimates(params1, params2, ipw_history, reward_history, action_history, state_history)
     return T.log(value_estimate_1_ - self.disagreement_value_1) + \
         T.log(value_estimate_2_ - self.disagreement_value_2)
+
+
+class SwitchingPolicyLearner(IteratedGameLearner):
+  """
+  Learn policy for detecting defections and switching to punishment policy.
+  """
+  def __init__(self,
+               cooperative_payoffs1,
+               punishment_params1,
+               default_params1,
+               default_params2,
+               payoffs1=np.array([[-1., -3.], [0., -2.]]),
+               payoffs2=np.array([[-1., -3.], [0., -2.]])):
+
+    super().__init__(payoffs1=payoffs1, payoffs2=payoffs2)
+    self.cooperative_payoffs1 = cooperative_payoffs1
+    self.default_params1 = default_params1
+    self.default_params2 = default_params2
+    self.punishment_params1 = punishment_params1  # Parameters for punishment policy
+
+  def learn(self,
+            label=0,
+            lr=0.1,
+            std=0.01,
+            n_epochs=2000,
+            n_print_every=None,
+            init_params1=None,
+            init_params2=None,
+            plot_learning=True
+            ):
+    super(SwitchingPolicyLearner, self).learn(label=label, lr=lr, updater1=updater1, updater2=updater2, std=std,
+                                               n_epochs=n_epochs, n_print_every=n_print_every, init_params1=init_params1,
+                                               init_params2=init_params2, plot_learning=plot_learning)
+
+    # Learn
+    for i in range(n_epochs):
+      print(i)
+      self.player2_has_defected = False
+
+      # Take action and observe rewards
+      if self.player2_has_defected:
+        # ToDo: assuming player 2 continues to follow default policy after defection is detected.
+        a1, a2, ipw = self.actions_from_params(self.punishment_params1, self.default_params2,
+                                               self.current_state[0], self.current_state[1])
+      else:
+        a1, a2, ipw = self.actions_from_params(self.default_params1, self.default_params2,
+                                               self.current_state[0], self.current_state[1])
+      r1, r2 = self.outcomes(a1, a2)
+
+      # Update histories
+      self.update_histories(r1, r2, a1, a2)
+      probs1 = T.sigmoid(params1[(2*a2):(2*a2 + 2)]).detach().numpy()
+      probs2 = T.sigmoid(params2[(2*a1):(2*a1 + 2)]).detach().numpy()
+      probs1 /= np.sum(probs1)
+      probs2 /= np.sum(probs2)
+      self.pr_CC_log[i] = probs1[0]*probs2[0]
+      self.pr_DD_log[i] = probs1[1]*probs2[1]
+
+      # Get gradient(s) of value function(s)
+      # ToDo: this can probably be improved
+      if self.joint_optimization:
+        def V_1(p1p2):
+          p1, p2 = p1p2
+          V_1_ = self.payoffs(p1, p2, self.ipw_history, self.reward_history, self.action_history, self.state_history)
+          return V_1_.requires_grad_()
+        V_2 = None
+      else:
+        def V_1(p1p2):
+          p1, p2 = p1p2
+          V_1_, _ = self.payoffs(p1, p2, self.ipw_history, self.reward_history, self.action_history, self.state_history)
+          return V_1_.requires_grad_()
+
+        def V_2(p1p2):
+          p1, p2 = p1p2
+          _, V_2_ = self.payoffs(p1, p2, self.ipw_history, self.reward_history, self.action_history, self.state_history)
+          return V_2_.requires_grad_()
+
+      self.payoffs1_log[i] = self.payoffs1[a1, a2]
+      self.payoffs2_log[i] = self.payoffs2[a2, a1]
+
+      if n_print_every and i % n_print_every == 0:
+        print(f"Epoch {i + 1} of {n_epochs}; payoffs:\t{V1:.2f}\t{V2:.2f};\tPr[CC]:\t{pCC:.2f};\tPr[DD]:\t{pDD:.2f}")
+      # noinspection PyInterpreter
+
+      # Get each agent's update
+      update1, update2 = self.gradient_ascent(
+        lr,
+        params1,
+        params2,
+        updater1,
+        V_1,
+        updater2=updater2,
+        V_2=V_2
+      )
+
+      # Do updates
+      params1.data += update1
+      params2.data += update2
+
+    self.final_params = (params1, params2)
+    if plot_learning:
+      self.plot_last_learning(label)
+    return {'pr_CC':self.pr_CC_log, 'pr_DD': self.pr_DD_log, 'payoffs1': self.payoffs1_log,
+            'payoffs2': self.payoffs2_log}
 
 
 if __name__ == "__main__":
