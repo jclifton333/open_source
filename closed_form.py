06a772f4437d7642686e9f13cf0d1113384c2c0a
jesse <jclifton333@gmail.com>
2019-11-27 23:27:55 -0500

    report jacobian

--- closed_form.py
+++ closed_form.py
@@ -26,6 +26,7 @@ def get_transition_matrix(p_cc_1, p_dc_1, p_cc_2, p_dc_2):
 
 def get_game_hessian(payoffs_1, payoffs_2, gamma=0.9):
   def value_function_1(p):
+    # true_p = np.maximum(np.minimum(p, 1.), 0)
     true_p = expit(p)
     p_cc_1, p_dc_1, p_cc_2, p_dc_2 = true_p[0], true_p[1], true_p[2], true_p[3]
     transition_matrix = get_transition_matrix(p_cc_1, p_dc_1, p_cc_2, p_dc_2)
@@ -33,20 +34,30 @@ def get_game_hessian(payoffs_1, payoffs_2, gamma=0.9):
     return v1[0]
 
   def value_function_2(p):
+    # true_p = np.maximum(np.minimum(p, 1.), 0)
     true_p = expit(p)
     p_cc_1, p_dc_1, p_cc_2, p_dc_2 = true_p[0], true_p[1], true_p[2], true_p[3]
     transition_matrix = get_transition_matrix(p_cc_1, p_dc_1, p_cc_2, p_dc_2)
     v2 = np.dot(np.linalg.inv(np.eye(4) - gamma*transition_matrix), payoffs_2)
     return v2[0]
 
+  jac_1 = nd.Jacobian(value_function_1)
+  jac_2 = nd.Jacobian(value_function_2)
   hess_1 = nd.Hessian(value_function_1)
   hess_2 = nd.Hessian(value_function_2)
   hess = lambda p: np.vstack((hess_1(p)[:2, :], hess_2(p)[2:, :]))
-  return hess
+  jac = lambda p: np.hstack((jac_1(p)[0, :2], jac_2(p)[0, 2:]))
+  return hess, jac
+
+
+def gradient_info_at_p(p, payoffs_1, payoffs_2):
+  h, j = get_game_hessian(payoffs_1, payoffs_2)
+  print('eigenvals: {}'.format(np.linalg.eig(h(p))[0]))
+  print('gradient: {}'.format(j(p)))
+  return
 
 
 if __name__ == "__main__":
-  # ToDo: Somehow make parameter space unbounded? (this explains why eigenvals aren't all positive)
   stag_payoffs_1 = np.array([2, -1, 1, 1])
   stag_payoffs_2 = np.array([2, 1, -1, 1])
   pd_payoffs_1 = np.array([1, -1, 2, -3])
@@ -55,12 +66,6 @@ if __name__ == "__main__":
   chicken_payoffs_2 = np.array([0, 1, -1, -5])
   dummy_payoffs_1 = np.array([1, -1, 1, -1])
   dummy_payoffs_2 = np.array([-1, 1, -1, 1])
-  # stag_grad = get_game_hessian(stag_payoffs_1,stag_payoffs_2)
-  # print(np.linalg.eig(stag_grad(np.array([1, 0, 1, 0])))[0])
-  # pd_grad = get_game_hessian(pd_payoffs_1,pd_payoffs_2)
-  # print(np.linalg.eig(pd_grad(np.array([1, 1, 1, 1])))[0])
-  # chicken_grad = get_game_hessian(chicken_payoffs_1,chicken_payoffs_2)
-  # print(np.linalg.eig(chicken_grad(np.array([1, 1, 0, 0])))[0])
-  dummy_grad = get_game_hessian(dummy_payoffs_1,dummy_payoffs_2)
-  print(np.linalg.eig(dummy_grad(np.array([1, 1, 1, 1])))[0])
+  gradient_info_at_p([10, 10, -10, -10], chicken_payoffs_1, chicken_payoffs_2)
+  gradient_info_at_p([0, 0, 0, 0], dummy_payoffs_1, dummy_payoffs_2)
 
